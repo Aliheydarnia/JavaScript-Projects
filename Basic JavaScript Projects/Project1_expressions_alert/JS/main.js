a75d5038ff7df1a6665281c4949cd33f725d2d5d
Ali Heydarnia <aliheydarnia13@outlook.com>
2024-10-05 18:03:21 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+window.alert("Hello, World");
+var A="Hello World<br>";
+document.write(A);
+var Sent1="This is the beginning of the string";
+var Sent2=",and this is the end of the string";
+document.write(Sent1 + Sent2); //Display variables by document.write method
+document.write(8+6); // it is an expression
